0a030058b9e4f8b2b7c081c1d675ef381f88107f
Danah Kim <lmvicky@naver.com>
2018-05-29 13:57:13 +0000

    ate notification, agora eh chat

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  acts_as_reader
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :posts
+  has_many :comments
+  has_many :new_notifications
+end
